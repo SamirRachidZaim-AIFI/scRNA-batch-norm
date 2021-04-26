46e3859b799b3c075e3857271eb5670b93be97f0
Samir Rachid Zaim <samir.rachidzaim@alleninstitute.org>
2021-04-26 02:48:25 +0000

    add 1% in sampling for rare celltypes

--- code/quant_scMerge.R
+++ code/quant_scMerge.R
@@ -101,87 +101,90 @@ sample_cellTypes <- function(sce,size=100, stratified=FALSE){
     colnames(probs) <- c('cellType','Frequency')
     probs$prob <- probs$Frequency/sum(probs$Frequency)
     
-    prob_vec <- round(probs$prob *size,0)+1
+    prob_vec <- round(probs$prob *size,0)+round(size/100,0) #ensures 1% of rare cell types (prevents 0s) 
     
     if(stratified){
-        stratified_df <- strata(sce@colData, 'cellType', size=prob_vec, method='srswor')
+        stratified_df <- strata(sce@colData, 'cellType', 
+                                size=prob_vec, method='srswor')
 
     } else {
-        stratified_df <- strata(sce@colData, 'cellType', size=rep(round(size/13),13), method='srswor')
+        stratified_df <- strata(sce@colData, 'cellType', 
+                                size=rep(round(size/13),length(unique(sce$cellType))), 
+                                method='srswor')
     }
     new_metadf <- getdata(sce@colData, stratified_df)
-    new_sub.sce <- 
-       return()
+    return(sce[,sce$barcodes %in% new_metadf$barcodes])
 }
 
+# break
 
 
-cat('Batch ef')
-##################  ##################  ##################
-##################  ##################  ##################
+# cat('Batch ef')
+# ##################  ##################  ##################
+# ##################  ##################  ##################
 
 
-# library(pvca)
-source('/home/scMergeNormalization/scRNA-batch-norm/code/pvca.R')
+# # library(pvca)
+# source('/home/scMergeNormalization/scRNA-batch-norm/code/pvca.R')
 
-gdata_logcpm <- data.frame(assay(sce_obc,'logcpm'))
-gdata_norm <- data.frame(assay(sce_object,'normalized'))
-head(gdata)
-mdata <- info <- data.frame(sce_object@colData)
+# gdata_logcpm <- data.frame(assay(sce_obc,'logcpm'))
+# gdata_norm <- data.frame(assay(sce_object,'normalized'))
+# head(gdata)
+# mdata <- info <- data.frame(sce_object@colData)
 
-head(mdata)
-names(mdata)
-names(gdata_logcpm) <- gsub('X','', names(gdata_logcpm))
-names(gdata_norm) <- gsub('X','', names(gdata_norm))
+# head(mdata)
+# names(mdata)
+# names(gdata_logcpm) <- gsub('X','', names(gdata_logcpm))
+# names(gdata_norm) <- gsub('X','', names(gdata_norm))
 
-sid <- 'barcodes'
-batch.factors <- c('cellType','batch')
-threshold <- 0.8
-interaction=T
+# sid <- 'barcodes'
+# batch.factors <- c('cellType','batch')
+# threshold <- 0.8
+# interaction=T
 
-pvcaObj_logcpm <-runPVCA(gdata=gdata_logcpm, mdata=mdata, sid=sid, factors = batch.factors, 0.8, T)
+# pvcaObj_logcpm <-runPVCA(gdata=gdata_logcpm, mdata=mdata, sid=sid, factors = batch.factors, 0.8, T)
 
-pvcaObj_norm<-runPVCA(gdata=gdata_norm, mdata=mdata, sid=sid, factors = batch.factors, 0.8, T)
+# pvcaObj_norm<-runPVCA(gdata=gdata_norm, mdata=mdata, sid=sid, factors = batch.factors, 0.8, T)
 
 
-pvca_ratios <- data.frame(rbind(pvcaObj_logcpm,
-                               pvcaObj_norm)
-                         )
+# pvca_ratios <- data.frame(rbind(pvcaObj_logcpm,
+#                                pvcaObj_norm)
+#                          )
 
-pvca_ratios <- rbind(pvca_ratios, 
-                    pvca_ratios[2,] / pvca_ratios[1,]
+# pvca_ratios <- rbind(pvca_ratios, 
+#                     pvca_ratios[2,] / pvca_ratios[1,]
                         
-                    )
+#                     )
 
 
 
 
 
-##################  ##################  ##################
-##################  ##################  ##################
-library(scMerge)
-require(sparseMatrixStats)
-data('segList_ensemblGeneID', package = 'scMerge')
-ctl <- segList_ensemblGeneID$human$human_scSEG
-ctl <- intersect(row.names(sce), ctl)
-smp_ctl <- sample(ctl, 10)
+# ##################  ##################  ##################
+# ##################  ##################  ##################
+# library(scMerge)
+# require(sparseMatrixStats)
+# data('segList_ensemblGeneID', package = 'scMerge')
+# ctl <- segList_ensemblGeneID$human$human_scSEG
+# ctl <- intersect(row.names(sce), ctl)
+# smp_ctl <- sample(ctl, 10)
 
-hvg <- row.names(sce)[!row.names(sce)%in% ctl]
+# hvg <- row.names(sce)[!row.names(sce)%in% ctl]
 
-smp_hvg <- sample(hvg,10)
+# smp_hvg <- sample(hvg,10)
 
-### quantify variance reduction on stable genes 
-cor_across_genes <- function(sce, smp_ctl, assay='normalized'){
+# ### quantify variance reduction on stable genes 
+# cor_across_genes <- function(sce, smp_ctl, assay='normalized'){
 
-    batch1 <- sce[, sce$batch=='X001']
-    batch2 <- sce[, sce$batch=='X002']
+#     batch1 <- sce[, sce$batch=='X001']
+#     batch2 <- sce[, sce$batch=='X002']
 
-    b1_df <-batch1@assays@data[assay][[1]]
-    b2_df <-batch2@assays@data[assay][[1]]
+#     b1_df <-batch1@assays@data[assay][[1]]
+#     b2_df <-batch2@assays@data[assay][[1]]
 
    
-    sm_df1 <- b1_df[row.names(b1_df)%in% smp_ctl,]
-    sm_df2 <- b2_df[row.names(b2_df)%in% smp_ctl,]
+#     sm_df1 <- b1_df[row.names(b1_df)%in% smp_ctl,]
+#     sm_df2 <- b2_df[row.names(b2_df)%in% smp_ctl,]
 
-    return(cor(rowMeans2(sm_df1), rowMeans2(sm_df2)))
-}
+#     return(cor(rowMeans2(sm_df1), rowMeans2(sm_df2)))
+# }
